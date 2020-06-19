95db54785143f0534fd39d7d809a66188f542f1e
Koteshwar Rao Myneni <mkraofx@gmail.com>
2020-06-20 01:07:58 +0530

    http code added

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  searchtxt = '';
+  resulttxt = '';
 
-  constructor() {}
+  list: any[];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  search() {
+    this.resulttxt = 'match found';
+    this.http.get(`https://fakepizzarestapi.herokuapp.com/china?q=${this.searchtxt}`).subscribe(
+      (result: any[]) => {
+        this.list = result;
+      },
+      (err) => {}
+    );
+  }
 }
