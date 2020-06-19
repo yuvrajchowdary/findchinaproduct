22da46c1ecb88fc7ff03e014f79e56a5c161f90e
Koteshwar Rao Myneni <mkraofx@gmail.com>
2020-06-19 23:35:52 +0530

    base code

---
+++ src/app/explore-container/explore-container.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-explore-container',
+  templateUrl: './explore-container.component.html',
+  styleUrls: ['./explore-container.component.scss'],
+})
+export class ExploreContainerComponent implements OnInit {
+  @Input() name: string = "explorecontant";
+  searchtxt = '';
+  resulttxt = '';
+  constructor() { }
+
+  ngOnInit() {}
+
+  search() {
+    this.resulttxt="match found"
+  }
+
+}
